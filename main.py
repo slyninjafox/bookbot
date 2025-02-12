b1ff68dd3f296111bbb304a4b2c5645880857979
slyninjafox <john.stuckey@gmail.com>
2025-02-11 23:28:32 -0500

    analysis of wordcounts and letter frequencies

---
+++ main.py
@@ -0,0 +1,18 @@
+def main():
+    ltr_freq = {}
+    num_words = 0
+    with open("books/frankenstein.txt") as f:
+        file_contents = f.read().lower()
+        num_words += len(file_contents.split())
+        for c in file_contents:
+            ltr_freq[c] = ltr_freq.get(c,0)+1
+
+    print("--- Begin report of books/frankenstein.txt ---")
+    print(f"{num_words} words found in the document")
+    print("")
+    for ltr in ltr_freq:
+        if ltr.isalpha():
+            print(f"The '{ltr}' character was found {ltr_freq[ltr]} times")
+    print("--- End report ---")
+
+main()
