ef4b0d3bb7784e8f65e66b22cae57bd7a4302f6f
slyninjafox <john.stuckey@gmail.com>
2025-02-22 16:20:38 -0500

    ch3 #1 complete

--- main.py
+++ main.py
@@ -1,18 +1,36 @@
-def main():
-    ltr_freq = {}
-    num_words = 0
-    with open("books/frankenstein.txt") as f:
-        file_contents = f.read().lower()
-        num_words += len(file_contents.split())
-        for c in file_contents:
-            ltr_freq[c] = ltr_freq.get(c,0)+1
+from stats import (
+    get_num_words,
+    chars_dict_to_sorted_list,
+    get_chars_dict,
+)
 
-    print("--- Begin report of books/frankenstein.txt ---")
-    print(f"{num_words} words found in the document")
-    print("")
-    for ltr in ltr_freq:
-        if ltr.isalpha():
-            print(f"The '{ltr}' character was found {ltr_freq[ltr]} times")
-    print("--- End report ---")
- 
+
+def main():
+    book_path = "books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = get_num_words(text)
+    chars_dict = get_chars_dict(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    print_report(book_path, num_words, chars_sorted_list)
+
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+
+def print_report(book_path, num_words, chars_sorted_list):
+    print("============ BOOKBOT ============")
+    print(f"Analyzing book found at {book_path}...")
+    print("----------- Word Count ----------")
+    print(f"Found {num_words} total words")
+    print("--------- Character Count -------")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+
+    print("============= END ===============")
+
+
 main()
